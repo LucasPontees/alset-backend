e2fe02aafdaf457eceb283908afbdd92fe47db07
Lucas Pontees <eulpontes@gmail.com>
2024-03-12 11:09:32 -0400

    add migrate, add banco, add sequelize

---
+++ README.md
@@ -0,0 +1,11 @@
+```bash
+# Clonando o projeto
+$ git clone https://lucaspontes@dev.azure.com/lucaspontes/alset/_git/alset
+```
+
+```bash
+$ npm install 
+```
+```bash
+$ cp example.env .env 
+```
--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const express = require('express')
 //vamos declarar o uso do express pelo meio da const app
 const app = express()
 //porta configurada para rodar na porta 3000
-const porta = 3000
+const port = 3000
 //configuramos uma rota principal para renderizar o texto
 app.get('/', (req, res)=>{
-    res.send('ola mundo, estamos configurados no azuredevops')
+    res.send('ola mundo, estamos configurados no azure devops')
 })
+
 // porta sendo ouvida na porta em questão.
-app.listen(porta, ()=>{
-    console.log(`Servidor rodando na porta ${porta}`)
+app.listen(port, ()=>{
+    console.log(`Servidor rodando na porta ${port}`)
 })
